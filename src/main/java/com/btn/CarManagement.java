3e45b83fdf50bfedccf083392f8f296857a5f21b
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-08-27 15:45:42 +0100

    update from meeting

--- src/main/java/com/btn/CarManagement.java
+++ src/main/java/com/btn/CarManagement.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarManagement {
-    public List<Object> fullListCars = new ArrayList<>();
-    public static void addNewCar(Car newCar, double price){
 
+
+    public List<Object> fullListCars = new ArrayList<>();
+    // Full list of cars
+
+    public static void availableCars(){
+        // this is the for
 
     }
-    public static void removeCar(Car car){
+    public static void rentedCars(Car car){
         if(car.getRent().equals(Rent.AVAILABLE)){
             // Use is about to delete the car
         }
